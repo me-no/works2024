7b36990115c39e06df8d8c8926b9d4ad148a55d4
me-no <taros131astro@gmail.com>
2024-08-13 02:59:46 +0900

    create bonus animation

---
+++ secret_door/8c62ace4f9ef8ccd08ca6fb992a8524bb7dbdc0530654bd254c9da07a660949a/orisaku-main.js
@@ -0,0 +1,162 @@
+let scal = 2;
+let cwidth,cheight;
+let org_x = 5*scal,org_y = 7*scal;
+let count=0;
+let btflyImgs = [];
+let btflyLocs = [];
+let dice;
+
+let currentTime;
+
+// drastic sin curve
+let xspacing = scal; // Distance between each horizontal location
+let w; // Width of entire wave
+let psi = 0.0; // Start angle at 0
+let amplitude; // Height of wave
+let period = 180.0; // How many pixels before the wave repeats
+let dx; // Value for incrementing x
+let yvalues; // Using an array to store height values for the wave
+
+
+function preload () {
+    imgbk = loadImage("btfly-bk.png");
+    sasakia = loadImage("oomurasaki.png");
+    for(let i =0;i<8;i++) {
+        j = i+1;
+        let img = loadImage(`bt${j}.png`);
+        btflyImgs.push(img);
+    }
+    font = loadFont('../x12y16pxMaruMonica.ttf');
+}
+
+function setup () {
+    cwidth = imgbk.width/4*scal;
+    cheight = imgbk.height/4*scal;
+    createCanvas(cwidth,cheight);
+    background(208,233,234);
+    dice = int(random(0,btflyImgs.length));
+
+    textSize(16);
+    textAlign(RIGHT);
+
+    // drastic sine curve
+    dx = (TWO_PI / period) * xspacing;
+    w =  cwidth + xspacing;
+    yvalues = new Array(floor(w / xspacing)); 
+    sinHeight = cheight/5;
+    amplitude = sinHeight;
+
+    // layer
+    layer = createGraphics(cwidth,cheight);
+    layer.background(255,255,255,0);
+}
+
+function draw () {
+    currentTime = new Date();
+    currentMonth = currentTime.getMonth();
+    currentDay = currentTime.getDay();
+    currentHour = currentTime.getHours();
+    currentMin = currentTime.getMinutes();
+
+    /**
+     * Get mouse distance from the center of sketch
+    */
+
+    let dX = mouseX - (width / 2.0);
+    let dY = mouseY - (height / 2.0);
+
+    // sin curve 
+    noStroke();
+    //fill(177,237,253,100);
+    fill(208,233,234,100);
+    calcWave();
+
+    for (let x = 0; x < yvalues.length; x++) {
+        drasticx = int(x*xspacing/scal)*scal;
+        drasticy = int(yvalues[x]/scal)*scal;
+    
+        rect(drasticx, drasticy+cheight/2, scal, scal);
+        rect(drasticx, drasticy+cheight*1/9, scal, scal);
+        rect(drasticx, drasticy+cheight*8/9, scal, scal);
+    }
+
+    // for rhombus
+    rhombusr = 16*scal;
+    rhombusx = int(random(-rhombusr, cwidth)/scal)*scal;
+    rhombusy = int(random(-rhombusr, cheight)/scal)*scal;
+    rcolors = [255,255,255,100];
+    if(count==6){
+        makeRhombus(rhombusx, rhombusy, rhombusr, rcolors);
+        count=0;
+    }
+    count++;
+
+    // mask with rectangle
+    fill(208,233,234);
+    rect(0,0,10*scal,cheight);
+    rect(0, cwidth-10*scal, cwidth, 10*scal);
+    rect(cwidth-10*scal, cheight/2, 10*scal, cheight/2);
+
+    push();
+    translate(dX / 80, dY / 80);
+    image(imgbk,0,0,cwidth,cheight);
+    pop();
+
+    image(btflyImgs[dice],mouseX,mouseY,btflyImgs[dice].width/4*scal,btflyImgs[dice].height/4*scal);
+
+    // layer
+    image(layer, 0, 0);
+
+    push();
+    translate(dX/40, dY/40);
+    image(sasakia,0,0,cwidth,cheight);
+    pop();
+
+    textFont("Pixelify Sans");
+
+    //fill(255);
+    //stroke('#AE662C');
+    //text('OTAKARA discovered!', cwidth-10,cheight-28);
+    fill('#EDB979');
+    //textFont(font);
+    text(currentMonth + '/' + currentDay +' '+currentHour + '.' + currentMin, cwidth-10, cheight-12);
+
+}
+
+function calcWave() {
+    psi += 0.01;// 波の速さはここ
+  
+    // For every x value, calculate a y value with sine function
+    let x = psi;
+    for (let i = 0; i < yvalues.length; i++) {
+      yvalues[i] = sin(x) * amplitude;
+      x += dx;
+    }
+  }
+  
+function makeRhombus (x, y, r, color) {// xyは左上、rは大きさ、colorは透過色込み
+    //rectr = int(random(2, 15))*2-1;// 奇数で出力
+    for (i = 0; i < r; i++) {
+        ii = i*2+1;
+        j = (r - ii)/2;
+        l = r - j*2;
+        noStroke();
+        fill(color[0],color[1],color[2], color[3]);
+        for (k = 0; k<l; k++) {
+            rect(x+j*scal+k*scal, y+i*scal, scal, scal);
+            if(i!=r-1){
+                rect(x+j*scal+k*scal, y+2*r*scal-i*scal-scal*2, scal, scal);
+            }
+        }
+      }
+  }
+  
+  function mouseClicked() {
+    int_x = int(mouseX/scal)*scal;
+    int_y = int(mouseY/scal)*scal;
+    //console.log(btflyImgs[dice]);
+    layer.image(btflyImgs[dice], int_x, int_y, btflyImgs[dice].width/4*scal, btflyImgs[dice].height/4*scal);
+    //layer.image(bt1,int_x,int_y,2*scal,2*scal);
+    dice = int(random(0, btflyImgs.length));
+
+  }
