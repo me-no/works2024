5ce3c477884f3f6be2cb323b73008fe4f35c0a92
me-no <taros131astro@gmail.com>
2024-08-13 20:16:24 +0900

    use alpha for trying

--- secret_door/8c62ace4f9ef8ccd08ca6fb992a8524bb7dbdc0530654bd254c9da07a660949a/orisaku-main.js
+++ secret_door/8c62ace4f9ef8ccd08ca6fb992a8524bb7dbdc0530654bd254c9da07a660949a/orisaku-main.js
@@ -85,8 +85,8 @@ function draw () {
      * Get mouse distance from the center of sketch
     */
 
-    dX = mouseX - (width / 2.0)+beta*10;
-    dY = mouseY - (height / 2.0)+gamma*10;
+    dX = mouseX - (width / 2.0)+alpha*10;
+    dY = mouseY - (height / 2.0)+beta*10;
 
     // sin curve 
     noStroke();
