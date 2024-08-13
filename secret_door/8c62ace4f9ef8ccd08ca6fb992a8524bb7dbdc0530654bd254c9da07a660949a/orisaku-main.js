8c66cc656918dc6474babab5b471f592a7691029
me-no <taros131astro@gmail.com>
2024-08-13 19:13:42 +0900

    deviceorientation testing

--- secret_door/8c62ace4f9ef8ccd08ca6fb992a8524bb7dbdc0530654bd254c9da07a660949a/orisaku-main.js
+++ secret_door/8c62ace4f9ef8ccd08ca6fb992a8524bb7dbdc0530654bd254c9da07a660949a/orisaku-main.js
@@ -85,8 +85,8 @@ function draw () {
      * Get mouse distance from the center of sketch
     */
 
-    dX = mouseX - (width / 2.0)-beta;
-    dY = mouseY - (height / 2.0)-gamma;
+    dX = mouseX - (width / 2.0)+beta*10;
+    dY = mouseY - (height / 2.0)+gamma*10;
 
     // sin curve 
     noStroke();
